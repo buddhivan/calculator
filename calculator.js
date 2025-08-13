6ec00b9cfacef7aeaf6d252c29bc6ee99d33c0e6
Bunty <buddhidahegaonkar@gmail.com>
2025-08-13 17:54:47 +0530

    made changes to display numbers  on screen

--- calculator.js
+++ calculator.js
@@ -5,16 +5,30 @@ function handleClick(event) {
     } else if (innerText == "="){
         console.log("equal");
     } else {
-        console.log(innerText);
+        addToScreen(innerText);
     }
 }
 
 function handleLoad() {
-    const buttons = document.getElementsByClassName("button")
+    const buttons = document.getElementsByClassName("button");
     for ( let k = 0; k < buttons.length; k++) {
         buttons[k].addEventListener("click", handleClick);
     }
 
+}
+ 
+function addToScreen(text) {
+    const screen = document.getElementById("screen");
+    const lastChar = screen.innerText.charAt(screen.innerText.length - 1);
+    
+    if (text == "+" || text == "-" || text == "*" || text == "/" || text =="." ) { 
+        if (lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar == "*" || lastChar == "." || lastChar =="" ) {    
+            return;
+        }
+         
+    } 
+    
+    screen.innerText = screen.innerText + text;
 }
 
 window.onload = handleLoad;
