b0196db986cbe763e052d77da67be40efe94fbea
Buddhivan <buddhidahegaonkar@gmail.com>
2025-08-14 14:08:15 +0530

    add button to clear the screen

--- calculator.js
+++ calculator.js
@@ -1,34 +1,38 @@
+function handleLoad() {
+    const buttons = document.getElementsByClassName("button");
+    for (let k = 0; k < buttons.length; k++) {
+        buttons[k].addEventListener("click", handleClick);
+    }
+}
+
 function handleClick(event) {
     const innerText = event.target.innerText;
     if (innerText == "b"){
         console.log("delete");
     } else if (innerText == "="){
         console.log("equal");
+    } else if (innerText == "AC"){ 
+        clearScreen();
     } else {
         addToScreen(innerText);
     }
 }
 
-function handleLoad() {
-    const buttons = document.getElementsByClassName("button");
-    for ( let k = 0; k < buttons.length; k++) {
-        buttons[k].addEventListener("click", handleClick);
-    }
-
-}
- 
 function addToScreen(text) {
     const screen = document.getElementById("screen");
     const lastChar = screen.innerText.charAt(screen.innerText.length - 1);
-    
+
     if (text == "+" || text == "-" || text == "*" || text == "/" || text =="." ) { 
         if (lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar == "*" || lastChar == "." || lastChar =="" ) {    
             return;
         }
-         
-    } 
-    
+    }
     screen.innerText = screen.innerText + text;
+}
+
+function clearScreen() {
+    const screen =  document.getElementById("screen");
+    screen.innerText = "";
 }
 
 window.onload = handleLoad;
